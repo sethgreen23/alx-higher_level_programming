6e475ff7d46ab5a110be6077cd7a694d87d4daf5
sethgreen23 <dridi.chaith@gmail.com>
2023-10-31 06:58:48 -0700

    better writing

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,6 @@ for i in range(0, 10):
     for j in range(0, 10):
         if i == j or i > j:
             continue
-        print("{}{}".format(i, j), end='')
-        if (i * 10 + j) < 89:
-            print(', ', end='')
+        end = '' if (i * 10 + j) >= 89 else ', '
+        print("{}{}".format(i, j), end=end)
 print()
