9e52722428af7fe516af346d719972d7c9089b99
sethgreen23 <dridi.chaith@gmail.com>
2023-10-31 05:54:26 -0700

    indedation problem

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,7 +8,7 @@ if last_digit == 0:
     print(f"Last digit of {number:d} is {last_digit:d} and is 0", end='\n')
 elif last_digit > 5:
     print(f"Last digit of {number:d} is {last_digit:d} and is "
-    "greater than 5", end='\n')
+          "greater than 5", end='\n')
 elif last_digit < 5:
     print(f"Last digit of {number:d} is {last_digit:d} "
-    "and is less than 6 and not 0", end='\n')
+          "and is less than 6 and not 0", end='\n')
