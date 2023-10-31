112f50015e604766fdd16a8ca97a55c5cc53269a
sethgreen23 <dridi.chaith@gmail.com>
2023-10-31 08:59:32 -0700

    fix indent issue

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
     for c in str:
-        if 97 <= ord(c) <= 122:
-            print("{}".format(chr(ord(c) - 32)), end="")
-        else:
-            print("{}".format(c), end="")
+        print("{}".format(chr(ord(c) - 32)) if (97 <= ord(c) <= 122)
+              else "{}".format(c), end="")
+    print()
