833cddad9d6422167522604a6fcf1d18ddb36fa3
sethgreen23 <dridi.chaith@gmail.com>
2023-11-02 08:10:39 -0700

    task0: hope it works

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
-a = 1
-b = 2
-
 if __name__ == '__main__':
     from add_0 import add
+
+    a = 1
+    b = 2
     result = add(a, b)
+
     print(f"{a:d} + {b:d} = {result:d}")
