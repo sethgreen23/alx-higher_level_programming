b46480a6f6e92175e7ebdc1f0e3b70235328e776
sethgreen23 <dridi.chaith@gmail.com>
2023-11-02 12:51:16 -0700

    task2: fixed the bug of :

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,14 @@
 if __name__ == '__main__':
     import sys
     argv_length = len(sys.argv) - 1
-    argv_or_s = "arguments" if not argv_length == 1 else "argument"
-    argv_print = "{0:d} {1}:".format(argv_length, argv_or_s)
+    if not argv_length == 1:
+        if not argv_length == 0:
+            argv_or_s = "arguments:"
+        else:
+            argv_or_s = "arguments."
+    else:
+        argv_or_s = "argument:"
+    argv_print = "{0:d} {1}".format(argv_length, argv_or_s)
     print(argv_print)
     if argv_length > 0:
         values = sys.argv[1:]
