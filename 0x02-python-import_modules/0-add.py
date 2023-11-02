9e7c043d00714030aed6d548e2ddac234d9b70b3
sethgreen23 <dridi.chaith@gmail.com>
2023-11-02 08:09:24 -0700

    task0: it works

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -3,5 +3,7 @@
 a = 1
 b = 2
 
-if __name__ == '__main__'
-    print(f"{a:d} + {b:d} = {add_0.add(a, b):d}")
+if __name__ == '__main__':
+    from add_0 import add
+    result = add(a, b)
+    print(f"{a:d} + {b:d} = {result:d}")
