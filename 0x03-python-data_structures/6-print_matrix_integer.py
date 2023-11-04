c2de3c7c168ef646b5af86cc0045e48d5ccae850
sethgreen23 <dridi.chaith@gmail.com>
2023-11-04 06:13:22 -0700

    task6: if matrix exist

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    rows = len(matrix)
-    cols = len(matrix[0])
-    for row in range(rows):
-        for col in range(cols):
-            end_str = '\n' if col == cols - 1 else ' '
-            print("{:d}".format(matrix[row][col]), end=end_str)
+    if matrix:
+        rows = len(matrix)
+        cols = len(matrix[0])
+        for row in range(rows):
+            for col in range(cols):
+                end_str = '\n' if col == cols - 1 else ' '
+                print("{:d}".format(matrix[row][col]), end=end_str)
