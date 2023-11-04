9ca21295720ade5c4fc054f74abc6e086913b8aa
sethgreen23 <dridi.chaith@gmail.com>
2023-11-04 06:29:58 -0700

    task6: make sure the matrix is empty

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix:
+    if not (len(matrix) == 1 and len(matrix[0]) == 0):
         rows = len(matrix)
         cols = len(matrix[0])
         for row in range(rows):
