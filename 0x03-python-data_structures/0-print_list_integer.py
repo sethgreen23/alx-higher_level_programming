3f8fb89fbcbe32b278a1b4be44f6cbf9c95e9dce
sethgreen23 <dridi.chaith@gmail.com>
2023-11-04 06:05:53 -0700

    task0: print list integer

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -2,5 +2,4 @@
 def print_list_integer(my_list=[]):
     if my_list:
         for value in my_list:
-            format_string = "{0:d}".format(value)
-            print(format_string)
+            print("{:d}".format(value))
