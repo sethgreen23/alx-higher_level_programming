1048a323f99bb5c3bff8f637585bfed731edecf5
sethgreen23 <dridi.chaith@gmail.com>
2023-11-04 18:29:46 -0700

    stuf

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if not (not matrix or all(not row for row in matrix)):
-        for i, row in enumerate(matrix):
-            for idx, value in enumerate(row):
-                end_str = "\n" if idx == len(row) - 1 else " "
-                print("{:d}".format(value), end=end_str)
+    for row in matrix:
+        for a in range(len(row)):
+            end_str = "\n" if a == len(row) - 1 else " "
+            print("{:d}".format(row[a]), end=end_str)
