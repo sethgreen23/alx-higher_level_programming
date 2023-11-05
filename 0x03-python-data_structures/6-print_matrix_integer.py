b34d196f31c4cc511b282c4ad46543c4e95577b8
sethgreen23 <dridi.chaith@gmail.com>
2023-11-04 18:15:47 -0700

    task6: lksjkl

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     if not (not matrix or all(not row for row in matrix)):
-        rows = len(matrix)
-        for row in range(rows):
-            list_row = matrix[row]
-            for col in range(len(list_row)):
-                end_str = "\n" if col == len(list_row) - 1 else " "
-                print("{:d}".format(matrix[row][col]), end=end_str)
+        for i, row in enumerate(matrix):
+            for idx, value in enumerate(row):
+                end_str = "\n" if idx == len(row) - 1 else " "
+                print("{:d}".format(value), end=end_str)
