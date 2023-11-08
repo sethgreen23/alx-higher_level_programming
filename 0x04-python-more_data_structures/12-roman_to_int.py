ef01b208b5c3c066da61d42eedb95e92e4c59569
sethgreen23 <dridi.chaith@gmail.com>
2023-11-08 12:29:47 -0800

    task12: refactor

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -12,17 +12,18 @@ def roman_to_int(roman_string):
                 }
     result = 0
     for idx, value in enumerate(roman_string):
-        if (value == 'V') and (roman_string[idx - 1] == 'I') and idx - 1 > 0:
+        idx_valid = (idx - 1 > 0 or idx == 1)
+        if (value == 'V') and (roman_string[idx - 1] == 'I') and idx_valid:
             result += rom_dict['V'] - (2 * rom_dict['I'])
-        elif (value == 'X') and (roman_string[idx - 1] == 'I') and idx - 1 > 0:
+        elif (value == 'X') and (roman_string[idx - 1] == 'I') and idx_valid:
             result += rom_dict['X'] - (2 * rom_dict['I'])
-        elif (value == 'L') and (roman_string[idx - 1] == 'X') and idx - 1 > 0:
+        elif (value == 'L') and (roman_string[idx - 1] == 'X') and idx_valid:
             result += rom_dict['L'] - (2 * rom_dict['X'])
-        elif (value == 'C') and (roman_string[idx - 1] == 'X') and idx - 1 > 0:
+        elif (value == 'C') and (roman_string[idx - 1] == 'X') and idx_valid:
             result += rom_dict['C'] - (2 * rom_dict['X'])
-        elif (value == 'D') and (roman_string[idx - 1] == 'C') and idx - 1 > 0:
+        elif (value == 'D') and (roman_string[idx - 1] == 'C') and idx_valid:
             result += rom_dict['D'] - (2 * rom_dict['C'])
-        elif (value == 'M') and (roman_string[idx - 1] == 'C') and idx - 1 > 0:
+        elif (value == 'M') and (roman_string[idx - 1] == 'C') and idx_valid:
             result += rom_dict['M'] - (2 * rom_dict['C'])
         else:
             result += rom_dict[value]
