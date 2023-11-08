6672fc77cdf03461aa2c51f2f8e2c21ca2287d4d
sethgreen23 <dridi.chaith@gmail.com>
2023-11-08 09:44:31 -0800

    task9: use update

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/user/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = a_dictionary.copy()
-    keys = list(new_dict.keys())
+    new_dict = dict()
+    keys = list(a_dictionary.keys())
     for key in keys:
-        new_dict[key] *= 2
+        new_dict.update({key: a_dictionary[key] * 2})
     return (new_dict)
