8d3c85ba48496757914db38238694c9587299c51
sethgreen23 <dridi.chaith@gmail.com>
2023-11-08 08:54:57 -0800

    task7: update 1

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary.update({key : value})
+    if not type(key) == 'str':
+        key = str(key)
+    if not key in a_dictionary:
+        a_dictionary.update({key : value})
+    else:
+        a_dictionary[key] = value
     return (a_dictionary)
