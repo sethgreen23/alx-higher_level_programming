7b0365859091a0643945f2d3d72795e42cb0495a
sethgreen23 <dridi.chaith@gmail.com>
2023-11-08 09:39:44 -0800

    task9: copy

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/user/bin/python3
 def multiply_by_2(a_dictionary):
-    new_dict = dict(a_dictionary)
+    new_dict = a_dictionary.copy()
     keys = list(new_dict.keys())
     for key in keys:
         new_dict[key] *= 2
