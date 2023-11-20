384e7b383924689eb59b7253a27f52a3d829d95c
sethgreen23 <dridi.chaith@gmail.com>
2023-11-20 15:35:47 -0800

    slkjs

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,7 +6,7 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except Exception as ex:
-        print("Exception: {}".format(ex), file=sys.stderr)
+        print("Exception: {:s}".format(ex), file=sys.stderr)
         return (False)
     print()
     return (True)
