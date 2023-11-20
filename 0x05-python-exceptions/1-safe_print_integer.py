8c0da86f478ebd26a35b2afd02d285729c28858c
sethgreen23 <dridi.chaith@gmail.com>
2023-11-20 06:35:20 -0800

    task1: exception

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,7 +2,7 @@
 def safe_print_integer(value):
     try:
         if not isinstance(value, int):
-            raise Exception("{value} is not and integer".format(value))
+            raise Exception
         print("{:d}".format())
     except Exception:
         return (False)
