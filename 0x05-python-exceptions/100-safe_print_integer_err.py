3248f4c3ab70a046cc7d99b0c3bba65502136805
sethgreen23 <dridi.chaith@gmail.com>
2023-11-20 15:14:28 -0800

    task100: klj

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,7 +5,4 @@ def safe_print_integer_err(value):
     except (ValueError) as ex:
         print("Exception: {}".format(ex))
         return (False)
-    finally:
-        print()
-    print()
     return (True)
