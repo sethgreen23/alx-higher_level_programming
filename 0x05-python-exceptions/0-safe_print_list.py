1559fae44041faf0670861545befc3f7a9a78a46
sethgreen23 <dridi.chaith@gmail.com>
2023-11-20 04:17:32 -0800

    task0: delete else sstatmen

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,6 +8,5 @@ def safe_print_list(my_list=[], x=0):
             printed = printed + 1;
     except IndexError as e:
         return (printed)
-    else:
-        return(printed)
+    return (printed)
 
