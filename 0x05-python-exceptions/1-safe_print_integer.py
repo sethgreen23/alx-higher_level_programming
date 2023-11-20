65aca65ae80b3ef148f2648c072a2b8fa396fd4f
sethgreen23 <dridi.chaith@gmail.com>
2023-11-20 06:38:45 -0800

    task1: add value to format function

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -3,7 +3,7 @@ def safe_print_integer(value):
     try:
         if not isinstance(value, int):
             raise Exception
-        print("{:d}".format())
+        print("{:d}".format(value))
     except Exception:
         return (False)
     else:
