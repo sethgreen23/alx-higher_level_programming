11a3037be4c9b842f8267e186935deb229b68cb0
sethgreen23 <dridi.chaith@gmail.com>
2023-11-20 09:36:33 -0800

    task2: print nothing before returning th printed caracter number

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -8,6 +8,7 @@ def safe_print_list_integers(my_list=[], x=0):
             print("{:d}".format(my_list[i]), end="")
             printed += 1
     except IndexError:
-        pass
+        print("")
+        return (printed)
     print("")
     return (printed)
