ccd6d40de3096d1c926d5b911c40a44f0cf048d4
sethgreen23 <dridi.chaith@gmail.com>
2023-11-20 04:24:39 -0800

    task0: end print function

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     printed = 0
     try:
         for num in my_list[:x]:
-            print(str(num))
+            print(str(num), end='')
             printed = printed + 1;
     except IndexError as e:
         return (printed)
