32cb4b96a36ce9d0d863c704b25d003c5a52fa2d
sethgreen23 <dridi.chaith@gmail.com>
2023-11-20 05:00:36 -0800

    task0: test for negative number

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,9 +2,10 @@
 def safe_print_list(my_list=[], x=0):
     printed = 0
     try:
-        for num in my_list[:x]:
-            print(str(num), end='')
-            printed = printed + 1;
+        if x > 0:
+            for num in my_list[:x]:
+                print(str(num), end='')
+                printed = printed + 1;
     except IndexError as e:
         print("")
         return (printed)
