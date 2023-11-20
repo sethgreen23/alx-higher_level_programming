857553a65ea161e7eaa301c966da7e2e033e5913
sethgreen23 <dridi.chaith@gmail.com>
2023-11-20 05:15:15 -0800

    task0: print new line in the except if printed is positive

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,8 @@ def safe_print_list(my_list=[], x=0):
                 print(str(num), end='')
                 printed = printed + 1;
     except IndexError as e:
-        print("")
+        if printed > 0:
+            print("")
         return (printed)
     print("")
     return (printed)
