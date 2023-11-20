6b862d23632b7d89a3ed0926e931a9cfb1e02f6b
sethgreen23 <dridi.chaith@gmail.com>
2023-11-20 15:37:34 -0800

    lflkj

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,9 +5,8 @@ import sys
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
-    except Exception as ex:
+    except ValueError as ex:
         print("Exception: {}".format(ex), file=sys.stderr)
-        print()
         return (False)
     print()
     return (True)
