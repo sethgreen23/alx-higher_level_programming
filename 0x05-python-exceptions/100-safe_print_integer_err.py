dc45c81b5d1953f77ee3fa6e67de2aa8b2a22ec2
sethgreen23 <dridi.chaith@gmail.com>
2023-11-20 15:19:34 -0800

    task100 stderr

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+import sys
+
 def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except (ValueError) as ex:
-        print("Exception: {}".format(ex))
+        print("Exception: {}".format(ex), file=sys.stderr)
         return (False)
     return (True)
