3983d5d8519ecc80b8751edefc0f99822d4ffdf5
sethgreen23 <dridi.chaith@gmail.com>
2023-11-20 04:12:43 -0800

    task0: first try

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    printed = 0
+    try:
+        for num in my_list[:x]:
+            print(str(num))
+            printed = printed + 1;
+    except IndexError as e:
+        return (printed)
+    else:
+        return(printed)
+
