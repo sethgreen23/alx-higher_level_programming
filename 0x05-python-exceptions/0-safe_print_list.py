e8222f15059cc8bdc577daa1af452d76fe5642c1
sethgreen23 <dridi.chaith@gmail.com>
2023-11-20 05:03:25 -0800

    task0: print new line if the the x > 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -9,6 +9,7 @@ def safe_print_list(my_list=[], x=0):
     except IndexError as e:
         print("")
         return (printed)
-    print("")
+    if x > 0:
+        print("")
     return (printed)
 
