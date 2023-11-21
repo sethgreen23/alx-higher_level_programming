c431474d437ad29cc59116a9b6f765eb9b6b0528
sethgreen23 <dridi.chaith@gmail.com>
2023-11-21 10:08:57 -0800

    task3: first try

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/user/bin/python3
+"""Module squre"""
+
+
+class Square:
+    """
+    Class defining a square.
+
+    Attributes:
+        size (float): The size of the square.
+    """
+    def __init__(self, size=0):
+        """
+        Initilizes a square object.
+
+        Parameters:
+            size (float): The size of the square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Calculate the area of the square.
+
+        Return:
+            float: current square area.
+        """
+        return (self.__size**2)
