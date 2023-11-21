ca769d29fc3cb99025e95074a9e57105ae5065f2
sethgreen23 <dridi.chaith@gmail.com>
2023-11-20 16:24:34 -0800

    task101: first try

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def safe_function(fct, *args):
+    try:
+        return (fct(args))
+    except Exception as ex:
+        print("Exception: {}".format(ex))
+        return (None)
