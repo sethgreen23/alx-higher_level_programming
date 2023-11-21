eebac784365f871d0221c2024da1954af63f9f1e
sethgreen23 <dridi.chaith@gmail.com>
2023-11-21 08:33:44 -0800

    task1: define class with size instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/pyhon3
+"""Square module"""
+
+
+class Square:
+    """
+    A Class representing a square.
+
+    Attributes:
+        size: size of the square
+    """
+    def __init__(self, size):
+        """
+        Initilizes a Square object.
+
+        Parameters:
+            size: size of the square
+        """
+        self.__size = size
