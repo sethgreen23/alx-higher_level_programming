f6bdd3385c6d83dfecd882a52be3805931e456f8
sethgreen23 <dridi.chaith@gmail.com>
2023-11-21 18:51:52 -0800

    lkjlj

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -89,7 +89,7 @@ class Square:
                 print("#", end="")
             print()
 
-    def __repr__(self):
+    def __str__(self):
         """Print the square"""
         st = []
         if self.size == 0:
