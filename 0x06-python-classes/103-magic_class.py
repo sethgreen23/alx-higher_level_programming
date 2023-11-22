5e4a6e1a3af8c3e6d53f7faf88181f63ef1375d1
sethgreen23 <dridi.chaith@gmail.com>
2023-11-22 06:32:56 -0800

    last task

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Module MagicClass"""
+
+
+import math
+
+
+class MagicClass:
+    """
+    A class representing a magic
+
+    Attributes:
+        radius (float): The radius of the circle
+    """
+
+    def __init__(self, radius):
+        """
+        Initilizes a Cicle object.
+
+        Parameters:
+            radius (float): The radius of the circle
+        """
+
+        if type(radius) is not float:
+            raise TypeError("radius must be a num")
+        self._MagicClass__radius = radius
+
+    def area(self):
+        """Calculate the area of a circle"""
+        return self._MagicClass__radius**2 * math.pi
+
+    def circumference(self):
+        """Calculate circonference of a circle"""
+        return 2 * math.pi * self._MagicClass__radius
