06aadd79e776fcd49e6a6e47df15cad534e3ed01
sethgreen23 <dridi.chaith@gmail.com>
2023-11-27 07:51:55 -0800

    cange

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -83,6 +83,6 @@ class Rectangle:
         string = ""
 
         if self.height != 0 and self.width != 0:
-            string = "\n".join("#" * self.width
+            string += "\n".join("#" * self.width
                                 for _ in range(self.height))
         return string
