2f1266dd1ecc55bad69ca06cac17ce308b2a753f
sethgreen23 <dridi.chaith@gmail.com>
2023-11-27 07:40:25 -0800

    ljlkj

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -83,5 +83,5 @@ class Rectangle:
         string = ""
 
         if self.height != 0 and self.width != 0:
-            string = ("#" * self.width + '\n') * self.height
+            string += ("#" * self.width + '\n') * self.height
         return string
