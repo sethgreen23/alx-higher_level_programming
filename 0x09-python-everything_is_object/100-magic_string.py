a64e05a91fd0a0a30bad00d8c531716fdaf7f80f
sethgreen23 <dridi.chaith@gmail.com>
2023-11-28 12:35:55 -0800

    lskj

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    magic_string.counter = getattr(magic_string, "counter", 0)
-    return "BestSchool, " * (magic_string.counter - 1) + "BestSchool"
+def magic_string(M=[]):
+    M = ["BestSchool"]
+    return ", ".join(M)
