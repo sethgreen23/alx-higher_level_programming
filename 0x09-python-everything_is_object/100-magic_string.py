670624962e09903366b8e606bb767a265f19f4c9
sethgreen23 <dridi.chaith@gmail.com>
2023-11-28 12:19:08 -0800

    task100: change

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def magic_string():
-    magic_string.n = getattr(magic_string, "counter", 0)
-    return "BestSchool, " * (magic_string.n - 1) + "BestSchool"
+    magic_string.counter = getattr(magic_string, "counter", 0)
+    return "BestSchool, " * (magic_string.counter - 1) + "BestSchool"
