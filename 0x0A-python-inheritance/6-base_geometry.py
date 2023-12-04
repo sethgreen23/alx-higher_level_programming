203cbf8003b39f23f12faf3c1638e7c94725cd4f
sethgreen23 <dridi.chaith@gmail.com>
2023-12-04 06:14:53 -0800

    task6: define area Method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Geometry Modul"""
+
+
+class BaseGeometry:
+    """
+    Class BaseGeometry
+    """
+
+    def area(self):
+        """Calculate the area of BaseGeometry"""
+        raise Exception("area() is not implemented")
