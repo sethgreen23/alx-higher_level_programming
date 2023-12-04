0c29eb37cdb12229d0847a249f99f76587684d19
sethgreen23 <dridi.chaith@gmail.com>
2023-12-04 05:42:02 -0800

    task3: is instance fo git

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Class or Inherit from"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Define if element is insance of the class or inherit from"""
+    return isinstance(obj, a_class)
