ef5254119330acbbf17a7f7a54aa60676f7dd942
sethgreen23 <dridi.chaith@gmail.com>
2023-12-04 09:26:39 -0800

    task8: change logic

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -49,8 +49,7 @@ class Rectangle(BaseGeometry):
 
         Return: Nothing
         """
-        if self.integer_validator("width", width):
-            self.__width = width
-
-        if self.integer_validator("height", height):
-            self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
