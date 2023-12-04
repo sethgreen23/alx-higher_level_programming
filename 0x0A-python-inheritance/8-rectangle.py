700326d242613c9983e1fba8efddc9b43686f6d4
sethgreen23 <dridi.chaith@gmail.com>
2023-12-04 10:15:17 -0800

    change logic

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -24,7 +24,7 @@ class BaseGeometry:
             Nothing
         """
 
-        if value.__class__ != int:
+        if type(value) is not int:
             raise TypeError(f"{name} must be an integer")
         if value <= 0:
             raise ValueError(f"{name} must be greater than 0")
