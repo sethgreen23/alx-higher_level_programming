27fc09c081c05cd6e60530df9dda90d6da7f1305
sethgreen23 <dridi.chaith@gmail.com>
2023-12-04 09:36:41 -0800

    task8: annoying

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -49,6 +49,9 @@ class Rectangle(BaseGeometry):
 
         Return: Nothing
         """
+        self.__width = 0
+        self.__height = 0
+
         self.integer_validator("width", width)
         self.__width = width
         self.integer_validator("height", height)
