eab691fdcf6c5e647bf17007bf6f2fa4570e7534
sethgreen23 <dridi.chaith@gmail.com>
2023-12-05 09:56:20 -0800

    task5: save to json file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""save to json file Module"""
+
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """Save to json file"""
+    with open(filename, mode="w", encoding="utf-8") as f:
+        f.write(json.dumps(my_obj)+'\n')
