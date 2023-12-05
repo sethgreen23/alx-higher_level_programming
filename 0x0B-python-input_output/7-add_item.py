a9fdd39942b9d929c0c2b90e7c6e411352c5621f
sethgreen23 <dridi.chaith@gmail.com>
2023-12-05 11:50:28 -0800

    task7: exept handler

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,16 +3,16 @@
 
 
 from sys import argv
-from os.path import isfile
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
 filename = "add_item.json"
-list_json = []
 arguments = argv[1:]
-if isfile(filename):
+try:
     list_json = load_from_json_file(filename)
+except FileNotFoundError:
+    list_json = []
 for element in arguments:
     list_json.append(element)
 save_to_json_file(list_json, filename)
