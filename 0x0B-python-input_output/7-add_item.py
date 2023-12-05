b2ac25fced77b5d43dad1114a4d048c98ad39dee
sethgreen23 <dridi.chaith@gmail.com>
2023-12-05 11:20:53 -0800

    task7: change logic

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,16 +3,15 @@
 
 
 import sys
+import os
 save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 
 list_json = []
-if len(sys.argv) > 1:
-    arguments = sys.argv[1:]
+arguments = sys.argv[1:]
+if os.path.isfile("add_item.json"):
     list_json = load_from_json_file("add_item.json")
-    for element in arguments:
-        list_json.append(element)
-    save_to_json_file(list_json, "add_item.json")
-else:
-    save_to_json_file(list_json, "add_item.json")
+for element in arguments:
+    list_json.append(element)
+save_to_json_file(list_json, "add_item.json")
