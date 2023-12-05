ad8dafaff84b8587900d9a9234b4b596f7c2667a
sethgreen23 <dridi.chaith@gmail.com>
2023-12-05 10:24:00 -0800

    task6: fload afrom json

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Load from json file Module"""
+
+
+import json
+
+
+def load_from_json_file(filename):
+    """Load from json file"""
+
+    with open(filename, mode="r", encoding="utf-8") as f:
+        line = f.readline()
+        return json.loads(line)
