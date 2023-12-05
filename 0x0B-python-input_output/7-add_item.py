aa818f50761cc0e48ba12132ebac958bc8906084
sethgreen23 <dridi.chaith@gmail.com>
2023-12-05 11:53:41 -0800

    task7: made me crazy

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,10 +9,16 @@ load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 filename = "add_item.json"
 arguments = argv[1:]
+
+
 try:
     list_json = load_from_json_file(filename)
 except FileNotFoundError:
     list_json = []
+
+
 for element in arguments:
     list_json.append(element)
+
+
 save_to_json_file(list_json, filename)
