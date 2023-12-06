b23ed1098d80908d3af76a295ec8f8a10f9d4a05
sethgreen23 <dridi.chaith@gmail.com>
2023-12-05 16:56:17 -0800

    task100: first try

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Append After"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Append After"""
+
+    lines_count = []
+    with open(filename, "r", encoding="utf-8") as f:
+        lst = list(f)
+    new_list = []
+    for line in lst:
+        new_list.append(line)
+        words = line.split(" ")
+        for word in words:
+            if search_string in word:
+                new_list.append(new_string)
+                break
+    with open(filename, "w", encoding="utf-8") as f:
+        for line in new_list:
+            f.write(line)
