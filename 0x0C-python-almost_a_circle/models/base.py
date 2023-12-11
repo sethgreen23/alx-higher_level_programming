73c1ba276d2549ee1394ccc48602709c35189128
sethgreen23 <dridi.chaith@gmail.com>
2023-12-11 07:34:39 -0800

    task15: to json string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Base class Module"""
 
+import json
+
 
 class Base:
     """
@@ -26,3 +28,13 @@ class Base:
     def set_nb_objects(cls):
         """Class method to set the value of nb_objest to zero"""
         Base.__nb_objects = 0
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        Static method that return string representation
+        of list_dictonaries
+        """
+        if list_dictionaries is None or not list_dictionaries:
+            return "[]"
+        return json.dumps(list_dictionaries)
--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -6,6 +6,8 @@ and Comme up with adequate testes
 """
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+import json
 
 
 class TestBaseIdValidValues(unittest.TestCase):
@@ -14,6 +16,10 @@ class TestBaseIdValidValues(unittest.TestCase):
     id valid values and make sure that we get
     good information out of it
     """
+
+    def setUp(self):
+        """Set up the start of objects"""
+        Rectangle.set_nb_objects()
 
     def test_id_two_classes(self):
         """
@@ -49,6 +55,41 @@ class TestBaseIdValidValues(unittest.TestCase):
         self.assertEqual(b2.id, -12)
         self.assertEqual(b3.id, -15)
         self.assertEqual(b4.id, 1)
+
+    def test_to_json_string(self):
+        """Test the to json string static method"""
+        r = Rectangle(10, 7, 2, 8)
+        dictionary = r.to_dictionary()
+        expected_output = json.dumps([dictionary])
+        actual_output = Base.to_json_string([dictionary])
+        self.assertEqual(expected_output, actual_output)
+
+        r = Rectangle(10, 7)
+        dictionary = r.to_dictionary()
+        expected_output = json.dumps([dictionary])
+        actual_output = Base.to_json_string([dictionary])
+        self.assertEqual(expected_output, actual_output)
+
+        r = Rectangle(10, 7, 2, 8, 13)
+        dictionary = r.to_dictionary()
+        expected_output = json.dumps([dictionary])
+        actual_output = Base.to_json_string([dictionary])
+        self.assertEqual(expected_output, actual_output)
+
+        dictionary = []
+        actual_output = Base.to_json_string([dictionary])
+        expected_output = json.dumps([dictionary])
+        self.assertEqual(expected_output, actual_output)
+
+        dictionary = [{}]
+        actual_output = Base.to_json_string([dictionary])
+        expected_output = json.dumps([dictionary])
+        self.assertEqual(expected_output, actual_output)
+
+        dictionary = [{}, {"age": 23, "name": "Arsene"}, {}]
+        actual_output = Base.to_json_string([dictionary])
+        expected_output = json.dumps([dictionary])
+        self.assertEqual(expected_output, actual_output)
 
 
 if __name__ == '__main__':
