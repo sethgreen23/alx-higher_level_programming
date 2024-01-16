9e1fb00e580a6d6a76c800093bd3c05511c9719d
sethgreen23 <dridi.chaith@gmail.com>
2024-01-16 08:01:28 -0800

    best score

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,2 +1,2 @@
 -- lists all record with score >= 10
-SELECT score, name from second_table WHERE score >= 10 ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
