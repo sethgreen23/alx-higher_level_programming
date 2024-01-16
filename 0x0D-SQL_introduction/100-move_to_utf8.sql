ff83aeac3d32d763cdf1368262775c02f7f9f9a5
sethgreen23 <dridi.chaith@gmail.com>
2024-01-16 10:13:52 -0800

    chang the table

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,4 @@
 -- convert hbtn_0c_0 database to UTF8
+use hbtn_0c_0
 ALTER TABLE first_table
-MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
