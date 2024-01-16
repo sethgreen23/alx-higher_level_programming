2369d69a4e898c13bc24b7b166b759a2c34047c5
sethgreen23 <dridi.chaith@gmail.com>
2024-01-16 09:16:27 -0800

    utf8

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- convert hbtn_0c_0 database to UTF8
 ALTER DATABASE hbtn_0c_0 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table COLLATE utf8mb4_unicode_ci
 ALTER INDEX name COLLATE utf8mb4_unicode_ci;
