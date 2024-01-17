c2d5cec08c6a8a108c9b55f12d656c1351f497d1
sethgreen23 <dridi.chaith@gmail.com>
2024-01-17 15:02:37 -0800

    all genres linked to thehat show from the datablse

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,2 @@
+-- Write a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows
+SELECT tv_shows.title AS title, tv_genres.name AS name FROM tv_shows LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id AND tv_genres.id IS NOT NULL ORDER BY title, name;
