e3ad8e6d69ac85b35c06f2de896485a609365541
Dridi Chaith <dridi.chaith@gmail.com>
2024-02-12 08:49:39 -0800

    task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let counter = 0;
+while (counter < arr.length) {
+  console.log(arr[counter]);
+  counter++;
+}
