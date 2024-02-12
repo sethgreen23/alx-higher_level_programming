70831bfa361250903d7ae975acca274a0acb0cd7
Dridi Chaith <dridi.chaith@gmail.com>
2024-02-12 12:15:25 -0800

    task 11 with another view

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,38 +1,20 @@
 #!/usr/bin/node
 
-function maxValue (arr) {
-  let maxV = parseInt(arr[2]);
-  for (let i = 3; i < arr.length; i++) {
-    const current = parseInt(arr[i]);
-    if (current > maxV) {
-      maxV = current;
-    }
-  }
-  return (maxV);
-}
-
-function secondValue (arr) {
-  const maxV = maxValue(arr);
-  let i = 2;
-  while (parseInt(args[i]) === maxV) {
-    i++;
-  }
-  let second = parseInt(arr[i]);
-  i++;
-  for (; i < arr.length; i++) {
-    const current = parseInt(arr[i]);
-    if (current === maxV) {
-      continue;
-    }
-    if (current > second) {
-      second = current;
-    }
-  }
-  return (second);
-}
 const args = process.argv;
 if (args.length <= 3) {
   console.log('0');
 } else {
-  console.log(`${secondValue(args)}`);
+  const copyArr = [];
+  for (let i = 2, j = 0; i < args.length; i++) {
+    copyArr[j++] = parseInt(args[i]);
+  }
+  const maxValue = Math.max(...copyArr);
+  const finalArr = [];
+  for (let i = 0, j = 0; i < copyArr.length; i++) {
+    if (copyArr[i] === maxValue) {
+      continue;
+    }
+    finalArr[j++] = copyArr[i];
+  }
+  console.log(`${Math.max(...finalArr)}`);
 }
