094081ac322bd65b1f5bad78877c5a89f75ebe7f
Dridi Chaith <dridi.chaith@gmail.com>
2024-02-12 09:34:55 -0800

    task 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+function add(a, b) {
+  if (isNaN(a) || isNaN(b)) {
+    console.log('NaN');
+  } else {
+    a = parseInt(a);
+    b = parseInt(b);
+    console.log(`${a + b}`);
+  }
+}
+
+const args = process.argv;
+const a = args[2];
+const b = args[3];
+add(a, b);
