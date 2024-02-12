2cccb5c155cf332d5026e58f5e024ee576d7a627
Dridi Chaith <dridi.chaith@gmail.com>
2024-02-12 08:44:17 -0800

    task 6

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,5 +4,5 @@ const args = process.argv;
 if (!args[2] || isNaN(parseInt(args[2]))) {
   console.log('Not a number');
 } else {
-  console.log(parseInt(args[2]));
+  console.log(`My number: ${parseInt(args[2])}`);
 }
