1f0ee90508b84bdfc9070cae055183cc65c7be99
Dridi Chaith <dridi.chaith@gmail.com>
2024-02-13 07:50:41 -0800

    task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,43 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const temp = this.width;
+    this.width = this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.height *= 2;
+    this.width *= 2;
+  }
+}
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (!c) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+};
