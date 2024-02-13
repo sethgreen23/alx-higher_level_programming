674f7430d103ede538b5328f7dc32dcad8e89772
Dridi Chaith <dridi.chaith@gmail.com>
2024-02-13 06:55:14 -0800

    task 3

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -7,4 +7,10 @@ module.exports = class Rectangle {
       this.height = h;
     }
   }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
 };
