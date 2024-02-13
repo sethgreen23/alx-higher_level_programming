1085ca33b62b48b9d36f2eb36b251b7b996393d1
Dridi Chaith <dridi.chaith@gmail.com>
2024-02-13 09:04:35 -0800

    task 8

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,5 +1,9 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  return list.reverse();
+  const revList = []
+  for (let i = list.length - 1; i >= 0; i--) {
+    revList.push(list[i]);
+  }
+  return revList;
 };
