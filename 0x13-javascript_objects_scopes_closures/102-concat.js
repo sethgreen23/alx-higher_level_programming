4d7e8ba889920f0f0de0a20d5adbc404fa45a62a
Dridi Chaith <dridi.chaith@gmail.com>
2024-02-13 12:05:39 -0800

    task 13

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,20 +1,23 @@
 #!/usr/bin/node
 
 const fs = require('fs');
-
+const args = process.argv;
+const fileA = args[2];
+const fileB = args[3];
+const fileC = args[4];
 // Read from fileA
-fs.readFile('fileA', (err, dataA) => {
+fs.readFile(fileA, (err, dataA) => {
   if (err) throw err;
 
   // Read from fileB
-  fs.readFile('fileB', (err, dataB) => {
+  fs.readFile(fileB, (err, dataB) => {
     if (err) throw err;
 
     // Concatenate contents of fileA and fileB
     const concatenatedData = dataA.toString() + dataB.toString();
 
     // Write to fileC
-    fs.writeFile('fileC', concatenatedData, (err) => {
+    fs.writeFile(fileC, concatenatedData, (err) => {
       if (err) throw err;
     });
   });
