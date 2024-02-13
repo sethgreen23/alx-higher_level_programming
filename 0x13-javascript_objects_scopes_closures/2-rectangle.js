cb5ca6a847452a563110a18aad873fc719713514
Dridi Chaith <dridi.chaith@gmail.com>
2024-02-13 06:38:59 -0800

    task 2 finished

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,12 +2,9 @@
 
 module.exports = class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
-      this.width = undefined;
-      this.height = undefined;
-    } else {
+    if (w > 0 && h > 0 && Number.isInteger(w) && Number.isInteger(h)) {
       this.width = w;
-      this.height = h;
+      this.height = w;
     }
   }
 };
