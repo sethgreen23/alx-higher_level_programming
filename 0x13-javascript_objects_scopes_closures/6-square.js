8bb424f8fe29bd0f06f2a960a8e26c3ea7ff442b
Dridi Chaith <dridi.chaith@gmail.com>
2024-02-13 08:12:53 -0800

    task6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -32,12 +32,16 @@ module.exports = class Square extends Rectangle {
   }
 
   charPrint (c) {
-    if (c === undefined) {
+    if (!c || !this.isCharacter(c)) {
       this.print();
     } else {
       for (let i = 0; i < this.height; i++) {
         console.log(c.repeat(this.width));
       }
     }
+  }
+
+  isCharacter (char) {
+    return (/[a-zA-Z]/).test(char);
   }
 };
