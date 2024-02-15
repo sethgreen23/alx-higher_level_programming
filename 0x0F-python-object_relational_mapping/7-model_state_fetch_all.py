ec92d3f7830703bc47b6b46313350b2c69803142
sethgreen23 <dridi.chaith@gmail.com>
2024-02-15 14:27:28 -0800

    task 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" Fetch all modle """
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    format_string = 'mysql+mysqldb://{}:{}@localhost/{}'
+    engine = create_engine(format_string.format(sys.argv[1],
+                           sys.argv[2],
+                           sys.argv[3]),
+                           pool_pre_ping=True)
+    Session = sessionmaker(bind = engine)
+    session = Session()
+    result = session.query(State).order_by(State.id.asc()).all()
+    for row in result:
+        print(f"{row.id}: {row.name}")
+
---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Model state module """
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+Base = declarative_base()
+
+
+class State(Base):
+    __tablename__ = 'states'
+
+    id = Column(Integer(), primary_key=True, unique=True, nullable=False)
+    name = Column(String(125), nullable=False)
