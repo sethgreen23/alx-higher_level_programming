79233c258c2a4d65e4c0c90a1912d5f728013e9a
sethgreen23 <dridi.chaith@gmail.com>
2024-02-15 20:21:42 -0800

    task1 change

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 """ list all states from database hbtn_0e_0_usa"""
 
+
+import sys
+import MySQLdb
+
+
 if __name__ == "__main__":
-    import sys
-    import MySQLdb
     USER = sys.argv[1]
     PASS = sys.argv[2]
     DB = sys.argv[3]
@@ -16,8 +19,8 @@ if __name__ == "__main__":
                            passwd=PASS,
                            db=DB, charset=CT)
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE states.name LIKE 'UCASE(N)%' \
-            ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE states.name LIKE 'N%' \
+            ORDER BY states.id ASC")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
