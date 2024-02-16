e40858aa0e38e1a4b73c5ad82a6ab1babc107739
sethgreen23 <dridi.chaith@gmail.com>
2024-02-15 19:04:41 -0800

    modely city rectify

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -6,6 +6,7 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship
 from model_state import Base
 
+
 class City(Base):
     __tablename__ = 'cities'
 
