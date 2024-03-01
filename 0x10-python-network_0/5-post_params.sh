aac431421620c6b518c0e2350deff17a13bc59c2
Dridi Chaith <dridi.chaith@gmail.com>
2024-02-29 16:48:19 -0800

    task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # diplays size of the body response
-curl -sLX POST -d "email=test@gmail.com&subjct=I will always be here for PLD" "$1"
+curl -sLX POST -d "email=test@gmail.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
