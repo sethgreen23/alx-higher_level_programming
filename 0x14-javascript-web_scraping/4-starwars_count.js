e723b7f5604aadd46c8a111a418998d250965754
Dridi Chaith <dridi.chaith@gmail.com>
2024-03-26 16:38:35 +0100

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -10,8 +10,8 @@ request(url, (error, response, body) => {
     let count = 0;
     for (let i = 0; i < res.length; i++) {
       const characters = res[i].characters;
-      for (const character in characters) {
-        if (character === '18') {
+      for (let j = 0; j < characters.length; j++) {
+        if (characters[j].includes('18')) {
           count++;
           break;
         }
