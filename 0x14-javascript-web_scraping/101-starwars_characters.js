0c889940a69191f1a2a8cf054eec1434421f5823
Dridi Chaith <dridi.chaith@gmail.com>
2024-03-26 18:51:45 +0100

    task 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -5,16 +5,43 @@ const args = process.argv;
 
 const id = args[2];
 const url = `https://swapi-api.alx-tools.com/api/films/${id}`;
-request(url, (error, response, body) => {
-  if (!error) {
-    const characters = JSON.parse(body).characters;
-    for (const character of characters) {
-      request(character, (err, resp, bod) => {
-        if (!err) {
-          const name = JSON.parse(bod).name;
-          console.log(name);
+
+// Function to fetch character name from URL
+const getCharacterName = (characterUrl) => {
+  return new Promise((resolve, reject) => {
+    request(characterUrl, (err, resp, bod) => {
+      if (err) {
+        reject(err);
+      } else {
+        const name = JSON.parse(bod).name;
+        resolve(name);
+      }
+    });
+  });
+};
+
+// Main function to fetch movie characters and print their names
+const printMovieCharacters = async () => {
+  try {
+    const movieResponse = await new Promise((resolve, reject) => {
+      request(url, (error, response, body) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(body);
         }
       });
+    });
+
+    const characters = JSON.parse(movieResponse).characters;
+    for (const character of characters) {
+      const characterName = await getCharacterName(character);
+      console.log(characterName);
     }
+  } catch (error) {
+    console.error('Error:', error);
   }
-});
+};
+
+// Call the main function
+printMovieCharacters();
