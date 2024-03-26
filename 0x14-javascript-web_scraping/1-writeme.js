7a6fb7279f2f6c89b7535ae43a95609afb6e5084
Dridi Chaith <dridi.chaith@gmail.com>
2024-03-26 15:23:19 +0100

    task 2

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -6,11 +6,10 @@ const args = process.argv;
 const fName = args[2];
 const content = args[3];
 if (fName) {
-  fs.writeFile(fName, content, 'utf-8', (err, data) => {
+  fs.writeFile(fName, content, 'utf-8', (err) => {
     if (err) {
       console.error(err);
       return;
     }
-    console.log(data);
   });
 }
