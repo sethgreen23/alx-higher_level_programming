fa8bab5585a37d49cc00a9687e33e22e30169262
Dridi Chaith <dridi.chaith@gmail.com>
2024-03-26 15:04:07 +0100

    task 0

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const args = process.argv;
+
+const fName = args[2];
+if (fName) {
+  fs.readFile(fName, 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(data);
+  });
+}
