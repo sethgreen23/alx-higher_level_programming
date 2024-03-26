fa8c346aff98611d6cd008c8a122899607c46653
Dridi Chaith <dridi.chaith@gmail.com>
2024-03-26 15:47:16 +0100

    task 2

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const request = require('request');
+const args = process.argv;
+
+const url = args[2];
+
+request(url, (error, response, body) => {
+  if (!error) { console.log('code: ', response.statusCode); } // Print the response body
+});
